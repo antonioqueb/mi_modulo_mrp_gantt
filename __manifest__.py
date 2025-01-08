70e9155dbecca3c4b842cb13b59b0cc2a0671ca1
Antonio Queb <antonioqueb@alphaqueb.online>
2025-01-07 19:11:16 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -1,21 +1,4 @@
-# -*- coding: utf-8 -*-
-
 {
-    'name': "MRP Gantt View",
-    'summary': """
-        Agrega una vista de Gantt a las órdenes de producción del módulo MRP.
-    """,
-    'description': """
-        Este módulo agrega una vista de Gantt a las órdenes de producción
-        del módulo MRP para una mejor visualización y gestión de la planificación.
-    """,
-    'author': "Tu Nombre",
-    'website': "http://www.tuweb.com",
-    'category': 'Manufacturing',
-    'version': '0.1',
-    'depends': ['mrp'],
-    'data': [
-        'views/mrp_production_views.xml',{
     'name': "MRP Gantt View",
     'summary': """
         Agrega una vista de Gantt a las órdenes de producción del módulo MRP.
@@ -38,6 +21,4 @@
             'mi_modulo_mrp_gantt/static/src/xml/mrp_dashboard.xml',
         ],
     }
-}
-    ],
 }
--- static/src/js/mrp_dashboard.js
+++ static/src/js/mrp_dashboard.js
@@ -6,9 +6,10 @@ import { AbstractAwaitableAction } from "@web/webclient/actions/abstract_awaitab
 class MrpDashboard extends AbstractAwaitableAction {
     setup() {
         super.setup();
+        // Aquí puedes agregar lógica adicional si la necesitas.
     }
 }
 
-MrpDashboard.tag = "mrp_production_dashboard";
+MrpDashboard.tag = "mrp_production_dashboard";  // Asegúrate que este tag coincida con lo que definiste en el views/mrp_production_views.xml
 
 registry.category("actions").add("mrp_production_dashboard", MrpDashboard);
