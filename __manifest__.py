04ffc8584866a0c3d59baa295cf79088bcb1c633
Antonio Queb <antonioqueb@alphaqueb.online>
2025-01-07 19:52:34 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -4,16 +4,25 @@
     'summary': 'Custom Gantt View for MRP',
     'description': 'This module adds a Gantt view to the MRP module.',
     'category': 'Manufacturing',
-    'author': 'Alphaqueb Consulting SAS',
+    'author': 'Your Name',
     'website': 'yourwebsite.com',
     'depends': ['mrp', 'web'],
     'data': [
+        'security/ir.model.access.csv',
         'views/mrp_production_views.xml',
     ],
     'assets': {
          'web.assets_backend': [
             'mi_modulo_mrp_gantt/static/src/js/mrp_dashboard.js',
         ],
+        'web.assets_frontend': [
+            'mi_modulo_mrp_gantt/static/src/js/mrp_dashboard.js',
+
+        ],
     },
     'license': 'LGPL-3',
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
+    },
 }
--- static/src/js/mrp_dashboard.js
+++ static/src/js/mrp_dashboard.js
@@ -1,20 +1,50 @@
- /** @odoo-module **/
+/** @odoo-module */
 
- import { registry } from "@web/core/registry";
- import { AbstractAwaitableAction } from "@web/webclient/actions/abstract_awaitable_action";
- import { useService } from "@web/core/utils/hooks";
+import { registry } from '@web/core/registry';
+import { Component, useState, onWillStart } from '@odoo/owl';
+import { useService } from '@web/core/utils/hooks';
+import { loadJS } from "@web/core/assets";
 
- export class MrpDashboard extends AbstractAwaitableAction {
-     setup() {
-         super.setup();
-         this.actionService = useService("action");
-     }
+export class MrpGanttDashboard extends Component {
+  setup() {
+    this.orm = useService("orm");
+    this.state = useState({
+         productions: [],
+    });
 
-     async start() {
-         await this.actionService.doAction('action_mrp_production_gantt');
-     }
- }
+      onWillStart(async () => {
+          await loadJS('/web/static/lib/frappe-gantt/frappe-gantt.min.js')
+           this.loadData()
+        });
 
- MrpDashboard.tag = "mrp_production_dashboard";
 
- registry.category("actions").add("mrp_production_dashboard", MrpDashboard);
+  }
+ async loadData(){
+    const records = await this.orm.searchRead('mrp.production', [], ['name','date_planned_start', 'date_planned_finished', 'progress']);
+        this.state.productions = records
+       this.renderGantt()
+  }
+
+
+  renderGantt(){
+    const tasks = this.state.productions.map(record => (
+            {
+                id: record.id,
+                name: record.name,
+                start: record.date_planned_start,
+                end: record.date_planned_finished,
+                progress: record.progress
+            }
+    ));
+      const gantt = new Gantt(".gantt-container", tasks,{
+         on_click: (task) => {
+            console.log(task)
+         }
+      });
+
+  }
+}
+
+MrpGanttDashboard.template = "mi_modulo_mrp_gantt.MrpGanttDashboard";
+
+registry.category("actions").add("mrp_gantt.mrp_gantt_dashboard", MrpGanttDashboard);
